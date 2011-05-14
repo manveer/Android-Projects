54a5a600ae6bcfc26b17fd7b3d09af398dc880d2
Manveer Chawla <manveer@bitmagic.in>
2011-05-14 21:35:19 +0530

    adding a startup activity to present the two options

---
+++ src/com/playground/farecalculator/activity/StartupActivity.java
@@ -0,0 +1,121 @@
+/**
+ * 
+ */
+package com.playground.farecalculator.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.playground.farecalculator.R;
+
+/**
+ * @author Manveer Chawla (manveer.chawla@gmail.com)
+ * 
+ */
+public class StartupActivity extends Activity implements OnClickListener, OnItemSelectedListener
+{
+	private Spinner citySpinner;
+	private Button startJourneyButton;
+	private Button estimateFareButton;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.start);
+		// The activity is being created.
+
+		initViews();
+		setListeners();
+	}
+
+	private void initViews()
+	{
+		citySpinner = (Spinner) findViewById(R.id.spinnerCity);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.cities_array,
+				android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		citySpinner.setAdapter(adapter);
+
+		startJourneyButton = (Button) findViewById(R.id.btnStartJourney);
+		startJourneyButton.setEnabled(true);
+
+		estimateFareButton = (Button) findViewById(R.id.btnEstimateFare);
+		estimateFareButton.setEnabled(true);
+	}
+
+	private void setListeners()
+	{
+		citySpinner.setOnItemSelectedListener(this);
+		startJourneyButton.setOnClickListener(this);
+		estimateFareButton.setOnClickListener(this);
+	}
+
+	@Override
+	protected void onStart()
+	{
+		super.onStart();
+		// The activity is about to become visible.
+	}
+
+	@Override
+	protected void onResume()
+	{
+		super.onResume();
+		// The activity has become visible (it is now "resumed").
+	}
+
+	@Override
+	protected void onPause()
+	{
+		super.onPause();
+		// Another activity is taking focus (this activity is about to be
+		// "paused").
+	}
+
+	@Override
+	protected void onStop()
+	{
+		super.onStop();
+		// The activity is no longer visible (it is now "stopped")
+	}
+
+	@Override
+	protected void onDestroy()
+	{
+		super.onDestroy();
+		// The activity is about to be destroyed.
+	}
+
+	public void onItemSelected(AdapterView<?> parent, View view, int pos, long id)
+	{
+		Toast.makeText(parent.getContext(), "The city is " + parent.getItemAtPosition(pos).toString(), Toast.LENGTH_LONG).show();
+	}
+
+	public void onNothingSelected(AdapterView parent)
+	{
+		// Do nothing.
+	}
+
+	@Override
+	public void onClick(View v)
+	{
+		switch (v.getId())
+		{
+		case R.id.btnEstimateFare:
+			break;
+		case R.id.btnStartJourney:
+			break;
+		default:
+			break;
+		}
+	}
+}
